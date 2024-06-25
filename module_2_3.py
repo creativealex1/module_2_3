354423ee59035c223fdea2ca8f66a4ea833244b8
creativealex1 <rudnik.2002@inbox.ru>
2024-06-25 18:01:02 +0300

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,8 +1,11 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 index = 0
 while index < len(my_list):
-    if my_list[index] < 0 :
+    num = my_list[index]
+    index = index + 1
+    if num == 0:
+        continue
+    elif num < 0:
         break
-    print(my_list[index])
-    index += 1
-
+    else:
+        print(num)
